0613727120f16d4125b584ce1eebe599c295ba2d
jainamshah245 <jainamshah245@gmail.com>
2022-02-22 02:09:01 +0530

    Created simple scripts

--- to_csv.py
+++ to_csv.py
@@ -75,10 +75,10 @@ def get_cf_graphql(start_date, end_date):
         "filter": {{
           "AND":[
             {{
-              "date_geq": "{start_date}"
+              "date_geq": "2022-02-20"
             }},
             {{
-              "date_leq": "{end_date}"
+              "date_leq": "2022-02-21"
             }}
           ]
         }}
@@ -89,60 +89,14 @@ def get_cf_graphql(start_date, end_date):
     return r
 
 
-#
-# def convert_to_csv(raw_data, start_date, end_date):
-#     data = pd.read_json(raw_data, dtype=False)['data']
-#     errors = pd.read_json(raw_data, dtype=False)['errors']
-#
-#     # Check if we got any errors
-#     if errors.notna().any() or not 'viewer' in data or not 'accounts' in data['viewer']:
-#         print('Failed to retrieve data: GraphQL API responded with error:')
-#         print(raw_data)
-#         return
-#
-#     # Flatten nested JSON data first
-#     network_analytics_normalized = pd.json_normalize(data['viewer']['accounts'], 'ipFlows1mAttacksGroups')
-#
-#     if len(network_analytics_normalized) == 0:
-#         print('We got empty response')
-#         return
-#
-#     network_analytics_abridged = network_analytics_normalized[[
-#         'dimensions.attackId',
-#         'min.datetimeMinute',
-#         'max.datetimeMinute',
-#         'dimensions.attackMitigationType',
-#         'dimensions.attackType',
-#         'dimensions.attackDestinationIP',
-#         'max.packetsPerSecond',
-#         'avg.packetsPerSecond']]
-#     # Rename the columns to get friendly names
-#     network_analytics_abridged.columns = [
-#         'Attack ID',
-#         'Started at',
-#         'Ended at',
-#         'Action taken',
-#         'Attack type',
-#         'Destination IP',
-#         'Max packets/second',
-#         'Avg packets/second']
-#     file = "{}network-analytics-{}-{}.csv".format(file_dir, start_date, end_date)
-#     network_analytics_abridged.to_csv(file)
-#     print("Successfully exported to {}".format(file))
-#
-#
-start_date = input("Enter start date and time in format of 2022-02-20T15:00:00Z ")
-end_date = input("Enter start date and time in format of 2022-02-21T14:59:59Z ")
+start_date = "2022-02-20T15:00:00.00"
+end_date = "2022-02-21T15:00:00.00"
 
 req = get_cf_graphql(start_date, end_date)
-# raw_data = req.text
-# print(raw_data)
+
 raw_data = json.loads(req.text)
+print(raw_data)
 data = raw_data["data"]["viewer"]["zones"][0]["firewallEventsAdaptive"]
-# if req.status_code == 200:
-#     convert_to_csv(req.text, start_date, end_date)
-# else:
-#     print("Failed to retrieve data: GraphQL API responded with {} status code".format(req.status_code))
 
 with open(f"firewall_activities.csv", newline='', mode='w') as file:
     output = csv.writer(file)
